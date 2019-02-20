651b44b43c1b0238b9b87cd4964f51fcccd2efee
Krista Blachian <krista.blachian@scifusion360.com>
2019-02-20 17:12:11 -0500

    initial commit

---
+++ src/com/theironyard/Main.java
@@ -0,0 +1,24 @@
+package com.theironyard;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    try {
+        Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Krista\\IdeaProjects\\testdb\\testjava.db");
+        Statement statement = conn.createStatement();
+        statement.execute("CREATE TABLE contacts(name TEXT, phone INTEGER, email TEXT)");
+
+        statement.close();
+        conn.close();
+
+    } catch(SQLException e) {
+        System.out.println("Something went wrong: " + e.getMessage());
+    }
+    }
+}
